57e9f4b1fb4751ea44b5b171061d337f1785f1ce
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-06-03 20:05:44 +0200

    Add new matcher for ZIP and similar named place

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use std::collections::HashMap;
 
 use askama::Template;
-use axum::{Json, Router};
 use axum::extract::Query;
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
+use axum::{Json, Router};
 use csv::{ReaderBuilder, StringRecord};
 use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use strsim::jaro_winkler;
 
 static AGS_CSV: &str = include_str!("resources/ags.csv");
+
+lazy_static! {
+    static ref PLZ_RE: Regex = Regex::new(r"^(?<plz>[0-9]{5})(\s+)(?<ort>.*)").unwrap();
+}
 
 #[derive(Serialize, Deserialize)]
 struct Entry {
@@ -22,7 +28,7 @@ struct Entry {
     #[serde(skip_serializing_if = "String::is_empty")]
     kreis: String,
     bundesland: String,
-    similarity: u8
+    similarity: u8,
 }
 
 impl Entry {
@@ -35,7 +41,7 @@ impl Entry {
             ort: record.get(2).unwrap().to_string(),
             kreis: record.get(3).unwrap().to_string(),
             bundesland: record.get(4).unwrap().to_string(),
-            similarity: 100
+            similarity: 0,
         }
     }
 }
@@ -45,37 +51,52 @@ impl Entry {
 struct IndexTemplate {
     query: String,
     multiple_assigned: Vec<String>,
-    entries: Vec<Entry>
+    entries: Vec<Entry>,
 }
 
 fn find_entries(query: String) -> Vec<Entry> {
-    let query = query.trim();
+    let query = query.trim().to_lowercase();
 
     if query.is_empty() {
-        return vec![]
+        return vec![];
     }
 
-    ReaderBuilder::new().from_reader(AGS_CSV.as_bytes())
+    ReaderBuilder::new()
+        .from_reader(AGS_CSV.as_bytes())
         .records()
         .filter(|record| record.is_ok())
         .map(|record| record.unwrap())
         .map(|record| Entry::from_record(record))
         .map(|mut entry| {
-            if format!("{} {}", entry.plz, entry.ort.to_lowercase()).starts_with(&query.to_lowercase()) {
+            if entry.plz.to_lowercase().starts_with(&query)
+                || entry.ort.to_lowercase().starts_with(&query)
+                || format!("{} {}", entry.plz, entry.ort.to_lowercase())
+                    .starts_with(&query)
+            {
                 entry.similarity = 100
-            } else {
-                entry.similarity = (100.0 * jaro_winkler(&query.to_lowercase(), &entry.ort.to_lowercase())) as u8
+            } else if match PLZ_RE.captures(&query) {
+                Some(caps) => {
+                    caps["plz"] == entry.plz
+                        && jaro_winkler(&caps["ort"], &entry.ort.to_lowercase()) >= 0.85
+                }
+                _ => false,
+            } {
+                entry.similarity = 100
+            } else if !PLZ_RE.is_match(&query) {
+                entry.similarity =
+                    (100.0 * jaro_winkler(&query, &entry.ort.to_lowercase())) as u8
             }
             entry
         })
-        .filter(|entry| entry.similarity > 90)
+        .filter(|entry| entry.similarity >= 90)
         .sorted_by(|e1, e2| e2.similarity.cmp(&e1.similarity))
         .take(25)
         .collect::<Vec<Entry>>()
 }
 
 fn find_multiple_assigned_zips() -> Vec<String> {
-    ReaderBuilder::new().from_reader(AGS_CSV.as_bytes())
+    ReaderBuilder::new()
+        .from_reader(AGS_CSV.as_bytes())
         .records()
         .filter(|record| record.is_ok())
         .map(|record| record.unwrap())
@@ -85,7 +106,7 @@ fn find_multiple_assigned_zips() -> Vec<String> {
         .chunk_by(|entry| entry.0.to_string())
         .into_iter()
         .map(|(zip, entries)| (zip, entries.unique().collect_vec()))
-        .filter(|(_, entries)| entries.len() > 1 )
+        .filter(|(_, entries)| entries.len() > 1)
         .into_iter()
         .map(|(a, _)| a)
         .unique()
@@ -94,36 +115,34 @@ fn find_multiple_assigned_zips() -> Vec<String> {
 
 async fn api_search(query: Query<HashMap<String, String>>) -> Response {
     if query.get("ma").unwrap_or(&String::new()).to_string() == "1" {
-        return Json::from(find_multiple_assigned_zips()).into_response()
+        return Json::from(find_multiple_assigned_zips()).into_response();
     }
 
-    let query = query.get("q").unwrap_or(&String::new()).to_string();
+    let query = query.get("q").unwrap_or(&String::new()).trim().to_string();
     Json::from(find_entries(query)).into_response()
 }
 
 async fn index(query: Query<HashMap<String, String>>) -> IndexTemplate {
     let multiple_assigned = if query.get("ma").unwrap_or(&String::new()).to_string() == "1" {
-       find_multiple_assigned_zips()
+        find_multiple_assigned_zips()
     } else {
         vec![]
     };
 
     let query = query.get("q").unwrap_or(&String::new()).to_string();
     IndexTemplate {
-        query: query.to_string(),
+        query: query.trim().to_string(),
         multiple_assigned,
-        entries: find_entries(query.to_string())
+        entries: find_entries(query.to_string()),
     }
 }
 
 #[tokio::main]
 async fn main() {
-
     let app = Router::new()
         .route("/", get(index))
         .route("/api", get(api_search));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap()
-
 }
